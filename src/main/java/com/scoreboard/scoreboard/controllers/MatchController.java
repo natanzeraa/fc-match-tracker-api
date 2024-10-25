a6260d45f155db1098c12c0ea0e27f8146a37387
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-25 15:42:36 -0300

    refactor: updated match controller now using service layer for communication

---
+++ src/main/java/com/scoreboard/scoreboard/controllers/MatchController.java
@@ -0,0 +1,61 @@
+package com.scoreboard.scoreboard.controllers;
+
+import com.scoreboard.scoreboard.models.Match;
+import com.scoreboard.scoreboard.dtos.MatchDTO;
+import com.scoreboard.scoreboard.services.MatchService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.List;
+
+
+@RestController
+@RequestMapping( "/match")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class MatchController {
+
+
+    final private MatchService matchService;
+
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Match>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(matchService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Match> findBYId(@PathVariable(value="id")UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(matchService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Match> create(@RequestBody @Valid MatchDTO matchDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(matchService.create(matchDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable(value = "id") UUID id, @RequestBody @Valid MatchDTO matchDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(matchService.update(id, matchDTO));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> patch(@PathVariable(value = "id") UUID id,
+                                        @RequestBody @Valid Map<String, Object> partialData){
+        return ResponseEntity.status(HttpStatus.CREATED).body(matchService.patch(id, partialData));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id){
+        matchService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
