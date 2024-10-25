71d0fe9a09e9eb8a67eee2b913f460df7b0f36c8
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-25 15:44:27 -0300

    feat: created player match model

---
+++ src/main/java/com/scoreboard/scoreboard/models/PlayerMatch.java
@@ -0,0 +1,57 @@
+package com.scoreboard.scoreboard.models;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serial;
+import java.util.UUID;
+import java.time.Instant;
+import java.io.Serializable;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="player_matches")
+public class PlayerMatch implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID=1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "home_player_id")
+    private Player homePlayer;
+
+    @ManyToOne
+    @JoinColumn(name = "away_player_id")
+    private Player awayPlayer;
+
+    @Column(nullable = false)
+    private Integer homePlayerGoals;
+
+    @Column(nullable = false)
+    private Integer awayPlayerGoals;
+
+    @ManyToOne
+    @JoinColumn(name = "match_id")
+    private Match match;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Instant createdOn;
+
+    @UpdateTimestamp
+    private Instant lastUpdate;
+}
