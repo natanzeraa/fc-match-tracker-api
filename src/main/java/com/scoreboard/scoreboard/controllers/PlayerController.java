12bf8eaa7ceb6fe48e93e3729478797be3c5293d
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-25 15:42:49 -0300

    refactor: updated player controller now using service layer for communication

---
+++ src/main/java/com/scoreboard/scoreboard/controllers/PlayerController.java
@@ -0,0 +1,62 @@
+package com.scoreboard.scoreboard.controllers;
+
+import com.scoreboard.scoreboard.models.Player;
+import com.scoreboard.scoreboard.dtos.PlayerDTO;
+import com.scoreboard.scoreboard.services.PlayerService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping( "/players")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class PlayerController {
+
+    PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Player>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(playerService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(playerService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Player> create(@RequestBody @Valid PlayerDTO playerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(playerService.create(playerDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable(value = "id") UUID id,
+                                         @RequestBody PlayerDTO playerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(playerService.update(id, playerDTO));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> patch(@PathVariable(value = "id") UUID id,
+                                         @RequestBody Map<String, Object> parcialData) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(playerService.patch(id, parcialData));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id) {
+        playerService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
