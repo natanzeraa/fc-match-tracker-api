2c426124c89118eebad6c1ce90e3b4181c182e5a
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-25 15:45:24 -0300

    feat: add player repository

---
+++ src/main/java/com/scoreboard/scoreboard/repositories/PlayerRepository.java
@@ -0,0 +1,13 @@
+package com.scoreboard.scoreboard.repositories;
+
+
+import com.scoreboard.scoreboard.models.Player;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, UUID> {}
+
