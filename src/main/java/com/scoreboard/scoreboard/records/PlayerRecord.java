fe455afd82a8abb8883238dc7b0a4149edfaf445
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-25 15:41:18 -0300

    refactor: removed unnecessary files

--- src/main/java/com/scoreboard/scoreboard/records/PlayerRecord.java
+++
@@ -1,6 +0,0 @@
-package com.scoreboard.scoreboard.records;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record PlayerRecord(@NotBlank String name, @NotNull String email) {}
