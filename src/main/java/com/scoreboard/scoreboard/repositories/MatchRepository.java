0552620e3467bde21eef4243ec9134e052205261
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-25 15:45:07 -0300

    refactor: reorganized imports

--- src/main/java/com/scoreboard/scoreboard/repositories/MatchRepository.java
+++ src/main/java/com/scoreboard/scoreboard/repositories/MatchRepository.java
@@ -1,11 +1,11 @@
 package com.scoreboard.scoreboard.repositories;
 
+import com.scoreboard.scoreboard.models.Match;
 import org.springframework.stereotype.Repository;
-import com.scoreboard.scoreboard.models.PlayerMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
+
 @Repository
-public interface PlayerMatchRepository extends JpaRepository<PlayerMatch, UUID> {
-}
+public interface MatchRepository extends JpaRepository<Match, UUID> { }
