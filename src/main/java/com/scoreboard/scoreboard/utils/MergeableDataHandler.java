5bc6b424822f166db03301d84d55c75346bee6c2
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-29 16:30:53 -0300

    style: reorganized imports and code styling

--- src/main/java/com/scoreboard/scoreboard/utils/MergeableDataHandler.java
+++ src/main/java/com/scoreboard/scoreboard/utils/MergeableDataHandler.java
@@ -1,28 +1,28 @@
 package com.scoreboard.scoreboard.utils;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 import org.springframework.util.ReflectionUtils;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Map;
-import java.lang.reflect.Field;
-
-
 public abstract class MergeableDataHandler {
-    public void mergeData(Map<String, Object> parcialData, Object classData) {
-        Class<?> targetClass = classData.getClass();
+	public void mergeData(Map<String, Object> parcialData, Object classData) {
+		Class<?> targetClass = classData.getClass();
 
-        ObjectMapper mapper = new ObjectMapper();
+		ObjectMapper mapper = new ObjectMapper();
 
-        Object convertedPlayerMatch = mapper.convertValue(parcialData, targetClass);
+		Object convertedPlayerMatch = mapper.convertValue(parcialData, targetClass);
 
-        parcialData.forEach((propertyName, propertyValue) -> {
-            Field field = ReflectionUtils.findField(targetClass, propertyName);
+		parcialData.forEach((propertyName, propertyValue) -> {
+			Field field = ReflectionUtils.findField(targetClass, propertyName);
 
-            if(field != null) {
-                field.setAccessible(true);
-                Object convertedFinalValue = ReflectionUtils.getField(field, convertedPlayerMatch);
-                ReflectionUtils.setField(field, classData, convertedFinalValue);
-            }
-        });
-    }
+			if (field != null) {
+				field.setAccessible(true);
+				Object convertedFinalValue = ReflectionUtils.getField(field, convertedPlayerMatch);
+				ReflectionUtils.setField(field, classData, convertedFinalValue);
+			}
+		});
+	}
 }
