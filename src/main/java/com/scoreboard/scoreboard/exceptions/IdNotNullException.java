37002e496f26679424c7ba609deb0dca0732928f
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-30 15:25:58 -0300

    feat: created id not null exception handler

---
+++ src/main/java/com/scoreboard/scoreboard/exceptions/IdNotNullException.java
@@ -0,0 +1,10 @@
+package com.scoreboard.scoreboard.exceptions;
+
+public class IdNotNullException extends RuntimeException {
+	private static final long serialVersionUID = -2954863082573840889L;
+
+	public IdNotNullException(String message) {
+		super(message);
+	}
+
+}
