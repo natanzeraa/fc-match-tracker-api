7ec3bee1abf8b270dcf8b63c104ac053222aa716
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-30 15:22:41 -0300

    style: reorganized imports

--- src/main/java/com/scoreboard/scoreboard/dtos/MatchDTO.java
+++ src/main/java/com/scoreboard/scoreboard/dtos/MatchDTO.java
@@ -1,11 +1,12 @@
 package com.scoreboard.scoreboard.dtos;
 
-import lombok.*;
-
 import java.util.UUID;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
